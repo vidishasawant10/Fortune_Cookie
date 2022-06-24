44f271a097dd0ebedd064be200fa9be4ce095544
sawantvidisha10 <sawantvidisha10@gmail.com>
2022-06-24 01:23:01 -0700

    updates

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import random
 
-print(random.randint(1,10))
-print(random.random()) #print values between 0 and 1
+print(random.randint(1, 10))
+print(random.random())  # print values between 0 and 1
 
-def random_ball():
-    ball = ["Yes", "No", "Maybe"]
+
+def fortune_cookie():
+    ball = ["You will have a good day", "You will have a dull day", "Your day can be good or bad"]
     return random.choice(ball)
 
-question = input("Is this the right answer? ")
-answer = print(random_ball())
+
+question = input("How will be my day today? ")
+answer = print(fortune_cookie())
+
+
+def lucky_number():
+    luckynumber = random.randint(1, 100)
+    print(f"Your lucky number for today is {luckynumber}")
+
+
+lucky_number()
+
 
 def random_ballgame():
-    answer = random.randint(1,3)
+    answer = random.randint(1, 3)
 
     if answer == 1:
-        print(answer,"Yes")
+        print(answer, "Yes")
     elif answer == 2:
-        print(answer,"No")
+        print(answer, "No")
     elif answer == 3:
-        print(answer,"Maybe")
+        print(answer, "Maybe")
+
 
 random_ballgame()
